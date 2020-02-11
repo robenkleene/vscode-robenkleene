f95b6d006342a93fe17d275a06a9332fcab41a3c
Roben Kleene <contact@robenkleene.com>
2020-02-11 15:39:34 -0500

    Enable script

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ set -e
 
 destination_path="$HOME/.vscode-oss/extensions/robenkleene"
 extension_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-rsync -a --delete -v --dry-run --exclude=install.sh --exclude=.gitignore \
+rsync -a --delete --verbose --exclude=install.sh --exclude=.gitignore \
   --exclude=.git "$extension_path" "$destination_path"
