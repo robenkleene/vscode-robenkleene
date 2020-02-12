2669f162cb53bcfae6b71e653026bc94da0621c9
Roben Kleene <contact@robenkleene.com>
2020-02-12 13:08:54 -0500

    Add backup file (#2)
    * Setup backup file
    
    * Setup spawn
    
    * Improve backup
    
    * Format

--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 		const child_process = require("child_process");
 		try {
-			child_process.execSync("~/.bin/urls_open", { input: text });
+			child_process.execFileSync("~/.bin/urls_open", null, { input: text });
 		}
 		catch (error) {
 			// Ignored, there's an error if no URLs are found.
@@ -53,7 +53,7 @@ export function activate(context: vscode.ExtensionContext) {
 		var path = require('path');
 		const currentDirPath = path.dirname(currentPath);
 		const destinationPath = path.resolve(currentDirPath, relativePath);
-		const fs = require("fs"); // Or `import fs from "fs";` with ESM
+		const fs = require("fs");
 		if (!fs.existsSync(destinationPath)) {
 			return;
 		}
@@ -65,6 +65,31 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(openFileDisposable);
+
+	let backupDisposable = vscode.commands.registerCommand('extension.backupFile', (uri: vscode.Uri) => {
+		const path = uri.fsPath;
+		const fs = require("fs");
+		if (!fs.existsSync(path)) {
+			return;
+		}
+		const child_process = require("child_process");
+		try {
+			// This doesn't return for some reason
+			// const result = child_process.execFileSync('~/.bin/backup_file', [path]);
+			// const message = result.toString();
+			const result = child_process.spawnSync('~/.bin/backup_file', [path], { shell: true });
+			const message = result.stdout.toString();
+			const error = result.stderr.toString();
+			if (message.length) {
+				vscode.window.showInformationMessage(message);
+			}
+			if (error.length) {
+				vscode.window.showInformationMessage(message);
+			}
+		}
+		catch (error) { }
+	});
+	context.subscriptions.push(backupDisposable);
 }
 
 export function deactivate() { }
