e65221e956b57fb14ce860954b4bd2be9ab8c5f2
Roben Kleene <contact@robenkleene.com>
2024-06-01 15:21:02 -0400

    Add open directory

--- src/extension.ts
+++ src/extension.ts
@@ -38,6 +38,45 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 	context.subscriptions.push(zDisposable);
+
+	let openFolderForFileDisposable = vscode.commands.registerCommand(
+		"robenkleene.openDirectory",
+		async (uri: vscode.Uri) => {
+		  var dirUri;
+		  var fileUri;
+		  const fs = require("fs");
+		  var path = require("path");
+		  if (uri) {
+			if (fs.lstatSync(uri.fsPath).isDirectory()) {
+			  // Use the selected directory in the file explorer
+			  dirUri = uri;
+			} else {
+			  fileUri = uri;
+			  const dirPath = path.dirname(fileUri.fsPath);
+			  dirUri = vscode.Uri.file(dirPath);
+			}
+		  } else {
+			// Or if a valid directory wasn't passed in, use the directory of the current file
+			const activeTextEditor = vscode.window.activeTextEditor;
+			if (!activeTextEditor) {
+			  return;
+			}
+			fileUri = activeTextEditor.document.uri;
+			let filePath = activeTextEditor.document.uri.fsPath;
+			const dirPath = path.dirname(filePath);
+			if (!fs.lstatSync(dirPath).isDirectory()) {
+			  return;
+			}
+			dirUri = vscode.Uri.file(dirPath);
+		  }
+
+		  if (!fs.existsSync(dirUri.fsPath)) {
+			return;
+		  }
+		  await vscode.commands.executeCommand("vscode.openFolder", dirUri);
+		}
+	  );
+	  context.subscriptions.push(openFolderForFileDisposable);
 }
 
 // This method is called when your extension is deactivated
