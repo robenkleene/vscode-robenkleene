10a45e4d9e590c6f3968c5b99585a848da6fe6d9
Roben Kleene <contact@robenkleene.com>
2024-06-01 08:22:09 -0400

    Setup Z

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,33 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let zDisposable = vscode.commands.registerCommand('robenkleene.z', async () => {
+		const result = await vscode.window.showInputBox({
+			placeHolder: "Z",
+		  });
+	
+		  if (!result?.length) {
+			return;
+		  }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "robenkleene" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('robenkleene.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from robenkleene!');
+		  const path = require("path");
+		  const os = require("os");
+	
+		  const destinationPath = result.fsPath;
+		  const fs = require("fs");
+		  if (
+			fs.existsSync(destinationPath) &&
+			fs.lstatSync(destinationPath).isDirectory()
+		  ) {
+			let destUri = vscode.Uri.file(destinationPath);
+			const success = await vscode.commands.executeCommand(
+			  "vscode.openFolder",
+			  destUri
+			);
+		  }	
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(zDisposable);
 }
 
 // This method is called when your extension is deactivated
