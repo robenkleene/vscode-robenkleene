5c3556aec7249ac40a4181144d32e25fded9c75e
Roben Kleene <contact@robenkleene.com>
2024-06-01 08:24:21 -0400

    Clean up

--- src/extension.ts
+++ src/extension.ts
@@ -5,28 +5,21 @@ export function activate(context: vscode.ExtensionContext) {
 		const result = await vscode.window.showInputBox({
 			placeHolder: "Z",
 		  });
-	
 		  if (!result?.length) {
 			return;
 		  }
-
-		  const path = require("path");
-		  const os = require("os");
-	
-		  const destinationPath = result.fsPath;
 		  const fs = require("fs");
 		  if (
-			fs.existsSync(destinationPath) &&
-			fs.lstatSync(destinationPath).isDirectory()
+			fs.existsSync(result) &&
+			fs.lstatSync(result).isDirectory()
 		  ) {
-			let destUri = vscode.Uri.file(destinationPath);
+			let destUri = vscode.Uri.file(result);
 			const success = await vscode.commands.executeCommand(
 			  "vscode.openFolder",
 			  destUri
 			);
 		  }	
 	});
-
 	context.subscriptions.push(zDisposable);
 }
 
