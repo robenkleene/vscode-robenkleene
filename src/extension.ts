55f7205cc9129bef9c185072632e68030f5abfc6
Roben Kleene <contact@robenkleene.com>
2024-06-01 12:16:15 -0400

    Work on Z

--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,46 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	let zDisposable = vscode.commands.registerCommand('robenkleene.z', async () => {
-		const result = await vscode.window.showInputBox({
+		const input = await vscode.window.showInputBox({
 			placeHolder: "Z",
-		  });
-		  if (!result?.length) {
+		});
+		if (!input?.length) {
 			return;
-		  }
-		  const fs = require("fs");
-		  if (
+		}
+
+		let result: string = "";
+		if (input) {
+			const { exec } = require('child_process');
+			exec(`zoxide query ${input}`, (error: { message: any; }, stdout: string, stderr: any) => {
+				if (error) {
+					vscode.window.showErrorMessage(`Error: ${error.message}`);
+					return;
+				}
+				if (stderr) {
+					vscode.window.showErrorMessage(`Error: ${stderr}`);
+					return;
+				}
+				result = stdout;
+			});
+		}
+
+		if (!result?.length) {
+			return;
+		}
+		const fs = require("fs");
+		if (
 			fs.existsSync(result) &&
 			fs.lstatSync(result).isDirectory()
-		  ) {
+		) {
 			let destUri = vscode.Uri.file(result);
 			const success = await vscode.commands.executeCommand(
-			  "vscode.openFolder",
-			  destUri
+				"vscode.openFolder",
+				destUri
 			);
-		  }	
+		}
 	});
 	context.subscriptions.push(zDisposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
