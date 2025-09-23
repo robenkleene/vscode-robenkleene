1e0984ea1d842194322ef5d616b1023f5fe4d1e7
Roben Kleene <contact@robenkleene.com>
2025-09-23 17:08:10 -0400

    Reapply "Remove Z command"
    This reverts commit 98f35854bc121f3c4b9d6f20d96917732a32689b.

--- src/extension.ts
+++ src/extension.ts
@@ -2,44 +2,6 @@ import * as vscode from 'vscode';
 import * as os from 'os';
 
 export function activate(context: vscode.ExtensionContext) {
-	let zDisposable = vscode.commands.registerCommand('robenkleene.z', async () => {
-		const input = await vscode.window.showInputBox({
-			placeHolder: "Z",
-		});
-		if (!input?.length) {
-			return;
-		}
-
-		let result: string = "";
-		if (input) {
-			const { execSync } = require('child_process');
-			try {
-				const output = execSync(`zoxide query ${input} | tr -d '\n'`);
-				result = output.toString();
-			} catch (error) {
-				const err = error as Error;
-				vscode.window.showErrorMessage(`Error: ${err.message}`);
-				return;
-			}
-		}
-
-		if (!result?.length) {
-			return;
-		}
-		const fs = require("fs");
-		if (
-			fs.existsSync(result) &&
-			fs.lstatSync(result).isDirectory()
-		) {
-			let destUri = vscode.Uri.file(result);
-			const success = await vscode.commands.executeCommand(
-				"vscode.openFolder",
-				destUri
-			);
-		}
-	});
-	context.subscriptions.push(zDisposable);
-
 	let openFolderForFileDisposable = vscode.commands.registerCommand(
 		"robenkleene.openDirectory",
 		async (uri: vscode.Uri) => {
